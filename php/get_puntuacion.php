031ca10adc3a848afe6a585165da9c68c757e7d4
Branko Straub <b.straub@outlook.com>
2015-08-26 14:46:51 -0300

    cambio php order by

--- php/get_puntuacion.php
+++ php/get_puntuacion.php
@@ -10,7 +10,7 @@ try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
-    $stmt = $conn->prepare("SELECT * FROM ".$tabla."");
+    $stmt = $conn->prepare("SELECT * FROM ".$tabla." order by puntajes.puntuacion desc" );
     $stmt->execute();
 
     // set the resulting array to associative
